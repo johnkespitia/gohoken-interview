5a5adff524e4a16af2e9d49472eae7bfdb47211b
johnespitia <jcespitia1@gmail.com>
2023-06-09 01:41:44 -0500

    conecting the environments

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,18 @@
-export default function Home() {
+import { getServerSideProps } from '@/services/client';
+
+const Home = ({ data }) => {
+    console.log(data)
   return (
     <main>
       <h1>Random Images!</h1>
       <div className="image-grid">
         <div className="image-item">
-          <img />
+          <img alt={"btw"} />
         </div>
       </div>
     </main>
   );
 }
+
+export { getServerSideProps };
+export default Home
---
+++ web/src/services/client.ts
@@ -0,0 +1,21 @@
+const  getServerSideProps = async () => {
+    try{
+    console.log(process.env.API_URL)
+    const response = await fetch(`${process.env.API_URL}/images`);
+    const data = await response.json();
+    return {
+        props: {
+            data,
+        },
+    };
+    }catch (e) {
+        console.log(e)
+        return {
+            props: {
+                data: null,
+            },
+        }
+    }
+}
+
+export {getServerSideProps}
