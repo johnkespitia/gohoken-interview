90b12a41e4c0843a6baaefab12ce9503b0684478
johnespitia <jcespitia1@gmail.com>
2023-06-15 21:39:31 -0500

    Implemented solution to the coding problem

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://gohoken-interview-front-app-1:3000',
+        // baseUrl: 'http://gohoken-interview-front-app-1:3000',
+        baseUrl: 'http://localhost:3000',
         supportFile: false,
         specPattern: "**/e2e/integration/*.spec.ts",
     },
